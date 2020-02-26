75da35a1a9267754be94e64394bbaf4ce79ae22f
Fran Figueroa <fmfp120@gmail.com>
2020-02-25 16:40:31 -0800

    Subida de Lista de Peliculas
    Subida tardía de la  Lista de Peliculas

---
+++ ListaPeliculas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPeliculas
+{
+    class ListPeliculas
+    {
+        private string Nombre;
+        public ListPeliculas(string Nombre) => this.Nombre = Nombre;
+    
+    public void Imprimir() => Console.WriteLine(Nombre);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ListPeliculas> Peliculas = new List<ListPeliculas>();
+            
+            Peliculas.Add(new ListPeliculas("El Irlandés"));
+            Peliculas.Add(new ListPeliculas("Gladiador"));
+            Peliculas.Add(new ListPeliculas("El Padrino"));
+            Peliculas.Add(new ListPeliculas("Pelotón"));
+            Peliculas.Add(new ListPeliculas("Rocky"));
+
+            foreach (ListPeliculas a in Peliculas)
+            {
+                a.Imprimir();
+            }
+        }
+    }
+}
