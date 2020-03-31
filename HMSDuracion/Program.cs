91c5895c72dd00b9d91e1e6ff14a30caf8f1027c
Fran Figueroa <fmfp120@gmail.com>
2020-03-30 18:24:21 -0700

    Inicio de Duracion

---
+++ HMSDuracion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HMSDuracion
+{
+    class Duracion
+    {
+        //Atributos de la Clase
+        int Horas, Mins, Segs;
+
+        //Creación del Constructor
+        public Duracion(int Horas, int Mins, int Segs);
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
